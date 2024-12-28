0e3baae356c77f182b29bed44815d038f3ae9d06
dm1nh <dangminhngo.dev@gmail.com>
2024-12-28 19:47:45 +0700

    fix: background colors

--- lua/trop/colors/hard.lua
+++ lua/trop/colors/hard.lua
@@ -2,8 +2,8 @@ local ret = vim.deepcopy(require("trop.colors.medium"))
 
 ---@type trop.Palette
 return vim.tbl_deep_extend("force", ret, {
-	bg = "#202529",
-	bg_dark = "#1e2226",
-	bg_dark1 = "#1a1f22",
-	bg_highlight = "#272d32",
+  bg = "#202529",
+  bg_dark = "#1c2024",
+  bg_dark1 = "#181c1f",
+  bg_highlight = "#272d32",
 })
